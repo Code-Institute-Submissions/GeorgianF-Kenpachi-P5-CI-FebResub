31f5e07797a59a968a81aca38eae4c0e5df6cfb8
GeorgianF <georgianfurtuna@yahoo.com>
2022-09-07 18:33:14 +0000

    add story.html and contact.html + link the urls

--- home/urls.py
+++ home/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='home')
+    path('', views.index, name='home'),
+    path('story/', views.story, name='story'),
+    path('contact/', views.contact, name='contact'),
 ]
--- home/views.py
+++ home/views.py
@@ -4,3 +4,13 @@ from django.shortcuts import render
 def index(request):
     """ A view to return the index page """
     return render(request, 'home/index.html')
+
+
+def story(request):
+    """ A view to return the story page """
+    return render(request, 'home/story.html')
+
+
+def contact(request):
+    """ A view to return the contact page """
+    return render(request, 'home/contact.html')
