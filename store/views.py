c66e15cbe706b0efd93d23c3b579a5d47d1e29ba
GeorgianF <georgianfurtuna@yahoo.com>
2022-09-24 22:06:20 +0000

    add pagination by 6 on the views in store

--- store/views.py
+++ store/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
 from .models import Product
+from django.core.paginator import Paginator
 
 
 def store(request):
     products = Product.objects.all()
-    context = {'products': products}
+    # Set up pagination
+    p = Paginator(Product.objects.all(), 6)
+    page = request.GET.get('page')
+    store_items = p.get_page(page)
+    
+    context = {
+        'products': products,
+        'store_items': store_items,
+        }
     return render(request, 'store/store.html', context)
