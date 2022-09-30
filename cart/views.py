f354a505e68fccf4270731cc18cff015772e2453
GeorgianF <georgianfurtuna@yahoo.com>
2022-09-30 06:33:35 +0000

    add functionnality to the cart view for auth user and non-auth user

--- cart/views.py
+++ cart/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
+from checkout.models import Order
 
 
 def cart(request):
-    context = {}
-    return render(request, 'cart/cart.html', context)
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(
+            customer=customer,
+            complete=False
+            )
+        items = order.orderitem_set.all()
+    else:
+        # Create empty cart for now for non-logged in user
+        items = []
+
+    context = {'items': items}
+    return render(request, 'store/cart.html', context)
