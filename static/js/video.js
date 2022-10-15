60b5a417283215f32134eadd983b3b85b8224f1c
GeorgianF <georgianfurtuna@yahoo.com>
2022-10-15 09:14:02 +0000

    add new path to the video.js to fix bug in the deployed version

--- static/js/video.js
+++ static/js/video.js
@@ -5,9 +5,10 @@ icon.addEventListener('click', () => {
     video.style.display = 'block';
     if (video.paused) {
         video.play();
-        icon.src = "media/pause.png";
+        icon.src = "media/pause.png" || "https://kenpachi-estore.s3.amazonaws.com/media/pause.png";
     } else {
         video.pause();
-        icon.src = "media/play.png";
+        icon.src = "media/play.png" || "https://kenpachi-estore.s3.amazonaws.com/media/play.png";
+        
     }
 })
