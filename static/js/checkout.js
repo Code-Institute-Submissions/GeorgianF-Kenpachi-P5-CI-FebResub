4204af48a4828fd0983340bc96d647f8ef5e06d2
GeorgianF <georgianfurtuna@yahoo.com>
2023-01-08 11:33:05 +0000

    test lighthouse in deployed site

--- static/js/checkout.js
+++ static/js/checkout.js
@@ -28,3 +28,56 @@ fetch("/checkout/config/")
         });
     });
   });
+
+
+// For users that are not authenticated
+// Future enhancement
+
+function submitFormData(event) {
+  var userData = {
+    'first_name': null,
+    'last_name': null,
+    'email': null,
+    'total': total,
+  };
+
+  userData.first_name = form.first_name.value;
+  userData.last_name = form.last_name.value;
+  userData.email = form.email.value;
+
+  fetch("/checkout/config/")
+    .then((result) => {
+      return result.json();
+    })
+    .then((data) => {
+      // Initialize Stripe.js
+      const stripe = Stripe(data.publicKey);
+
+      // Get Checkout Session ID
+      var url = "/checkout/create-checkout-session/";
+      fetch(url, {
+          method: "POST",
+          headers: {
+            'Content-Type': 'application/json',
+            'X-CSRFToken': csrftoken,
+          },
+          body: JSON.stringify({
+            'form': userData,
+          }),
+        })
+        .then((result) => {
+          return result.json();
+        })
+        .then((data) => {
+          console.log(data);
+          // Redirect to Stripe Checkout
+          return stripe.redirectToCheckout({
+            sessionId: data.sessionId
+          });
+        })
+        .then((res) => {
+          console.log(res);
+        });
+
+    });
+}
